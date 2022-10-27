f18f128ac2daa0f7967bd25933b6bdf896106a60
SerhiiNazarov <algazira89@gmail.com>
2022-10-27 22:18:07 +0300

    add onSearchClick

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './js/modal';
 import './js/gallery';
+import './js/onSearchForm';
--- src/js/utilitiesJS/murkupGalleryOnPageLoading.js
+++ src/js/utilitiesJS/murkupGalleryOnPageLoading.js
@@ -1,25 +1,27 @@
-import { refs } from "./refs";
-import { ServerApi } from "./serverApi";
+import { refs } from './refs';
+import { ServerApi } from './serverApi';
 
-const serverApi = new ServerApi;
+const serverApi = new ServerApi();
 
 export function murkupGalleryOnPageLoading(movies) {
-    const moviesMurkup = movies.map(movie => {
-        const { original_title, poster_path } = movie;
-        return `
+  const moviesMurkup = movies
+    .map(movie => {
+      const { original_title, poster_path } = movie;
+      return `
         <li class="film__item">
         <img src="https://image.tmdb.org/t/p/w500${poster_path}" class="film__img" alt="${original_title}" />
         <p class="film__title">${original_title}</p>
         <p class="film__genre">Drama, Action | 2020</p>
-      </li>`
-    }).join(``);
-    
-    return refs.gallery.insertAdjacentHTML(`beforeend`, moviesMurkup);
+      </li>`;
+    })
+    .join(``);
+
+  return refs.gallery.insertAdjacentHTML(`beforeend`, moviesMurkup);
 }
 
 export async function murkupGallery(params) {
-    const movies = await serverApi.getPopularMovie();
+  const movies = await serverApi.getPopularMovie();
 
-    murkupGalleryOnPageLoading(movies.results);
-    // console.log(movies.results);
+  murkupGalleryOnPageLoading(movies.results);
+  // console.log(movies.results);
 }
