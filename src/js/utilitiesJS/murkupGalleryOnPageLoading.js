c0423f65a041961c047bec5de37bccbf58d92245
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-10-27 17:17:27 +0300

    при клике на фильм получаем его детали

--- src/js/utilitiesJS/murkupGalleryOnPageLoading.js
+++ src/js/utilitiesJS/murkupGalleryOnPageLoading.js
@@ -5,10 +5,10 @@ const serverApi = new ServerApi;
 
 export function murkupGalleryOnPageLoading(movies) {
     const moviesMurkup = movies.map(movie => {
-        const { backdrop_path, original_title, poster_path } = movie;
+        const { original_title, poster_path } = movie;
         return `
         <li class="film__item">
-        <img src="https://image.tmdb.org/t/p/w500${poster_path}" class="film__img" />
+        <img src="https://image.tmdb.org/t/p/w500${poster_path}" class="film__img" alt="${original_title}" />
         <p class="film__title">${original_title}</p>
         <p class="film__genre">Drama, Action | 2020</p>
       </li>`
@@ -18,8 +18,8 @@ export function murkupGalleryOnPageLoading(movies) {
 }
 
 export async function murkupGallery(params) {
-    const movies = await serverApi.getMovie();
+    const movies = await serverApi.getPopularMovie();
 
     murkupGalleryOnPageLoading(movies.results);
-    console.log(movies.results);
+    // console.log(movies.results);
 }
--- src/js/utilitiesJS/serverApi.js
+++ src/js/utilitiesJS/serverApi.js
@@ -5,16 +5,26 @@ const KEY = `7770a554235a470dd8487676c4d97407`;
 export class ServerApi {
     baseUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${KEY}`;
 
-    async getMovie() {
+    async getPopularMovie() {
         const data = await axios ({
             url: this.baseUrl,
         });
 
-        const movies = await data.data;
+        return await data.data;
+    }
 
-        console.log(movies);
+    async getMovieOnDemand(query) {
+        const data = await axios ({
+            url: `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&page=1&include_adult=false&query=${query}`,
+        });
+        return await data.data.results;
+    }
 
-        return movies;
+    async getDetailsMovie(id) {
+        const data = await axios({
+            url: `https://api.themoviedb.org/3/movie/${id}?api_key=${KEY}&language=en-US`,
+        })
+        return await data.data;
     }
 }
 
