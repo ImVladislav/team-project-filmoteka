de331b69704c240966b83c9ec235caec3bcb4d5f
ImVladislav <103652143+ImVladislav@users.noreply.github.com>
2022-10-27 16:41:50 +0000

    Deploying to gh-pages from @ ImVladislav/team-project-filmoteka@1182b09826533a7ead7848dbfcac9e2acbe6c93a 🚀

--- index.e2cd8906.js
+++
@@ -1,2 +0,0 @@
-const e={overlay:document.querySelector(".modal-overlay"),content:document.querySelector(".modal-content"),gallery:document.querySelector(".gallery"),body:document.querySelector("body")};e.gallery.addEventListener("click",(function(){e.overlay.classList.add("visiable"),e.body.classList.add("no-scroll")})),e.overlay.addEventListener("click",(function(o){o.target!==e.content&&(e.overlay.classList.remove("visiable"),e.body.classList.remove("no-scroll"))}));
-//# sourceMappingURL=index.e2cd8906.js.map
