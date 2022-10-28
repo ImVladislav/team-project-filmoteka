ff32dd4e61895ad841b8d2a0eea0e7c7e55d7ffa
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-10-28 13:18:31 +0300

    movie details finished

---
+++ src/js/descriptionMurkup.js
@@ -0,0 +1,51 @@
+export const movieDescriptionMurkup = ({
+  id,
+  poster_path,
+  title,
+  original_title,
+  popularity,
+  overview,
+  genres,
+  vote_average,
+  vote_count,
+}) => {
+  const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+  const genreList = [];
+  genres.forEach(({ name }) => genreList.push(name));
+
+  return `
+    <div class="movie__description" data-movie-id='${id}'>
+    <div class="movie__poster-wrappaer">
+      <img
+        src="${baseImageUrl}${poster_path}"
+        alt="${title}"
+        class="movie__poster"
+       
+      />
+    </div>
+    <div class="movie__container">
+      <h3 class="movie__title">${title}</h3>
+      <div class="description__container">
+        <div class="description__title-wrapper">
+          <p class="description__title">Vote / Votes</p>
+          <p class="description__title">Popularity</p>
+          <p class="description__title">Original Title</p>
+          <p class="description__title">Genre</p>
+        </div>
+        <div class="description__text-wrapper">
+          <p class="description__text"><span class='vote vote-accent'>${vote_average}</span>&#47;<span class='vote'>${vote_count}</span></p>
+          <p class="description__text">${popularity}</p>
+          <p class="description__text">${original_title}</p>
+          <p class="description__text">${genreList.join(', ')}</p>
+        </div>
+      </div>
+      <h4 class="about__title">About</h4>
+      <p class="about__text">${overview}</p>
+      <div class="btn__container">
+        <button type="button" class="modal__btn">add to watched</button>
+        <button type="button" class="modal__btn">add to queue</button>
+      </div>
+    </div>
+  </div>
+  `;
+};
