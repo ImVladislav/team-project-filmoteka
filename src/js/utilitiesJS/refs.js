ad29a397a506cf8991b72cec543df21cb75e75d2
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-10-28 13:18:03 +0300

    movie details finished

--- src/js/utilitiesJS/refs.js
+++ src/js/utilitiesJS/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   gallery: document.querySelector(`.gallery`),
   formRef: document.querySelector('.header__form'),
+  movieDescription: document.querySelector('[data-movie-description]'),
 };
