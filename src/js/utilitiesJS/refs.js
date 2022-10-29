99884860978a2048c6cd10ce1bcd51caa6447bd0
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-10-29 05:54:49 +0300

    dark-theme

--- src/js/utilitiesJS/refs.js
+++ src/js/utilitiesJS/refs.js
@@ -5,4 +5,10 @@ export const refs = {
   gallery: document.querySelector(`.gallery`),
   formRef: document.querySelector('.header__form'),
   movieDescription: document.querySelector('[data-movie-description]'),
+
+  team: document.querySelector('.js-team-modal'),
+  wrap: document.querySelector('.modal-team-wrap'),
+  contentTeam: document.querySelector('.modal-team-content'),
+  overlayTeam: document.querySelector('.modal-team-overlay'),
+  btnClose: document.querySelector('.modal-team__close-btn'),
 };
