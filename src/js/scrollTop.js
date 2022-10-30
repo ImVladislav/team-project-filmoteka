4465f81ee0f3c605e8947ce848625148235f1853
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-10-30 11:30:45 +0200

    ScrollTo

--- src/js/scrollTop.js
+++ src/js/scrollTop.js
@@ -1,15 +1,16 @@
 import { refs } from './utilitiesJS/refs';
 
 window.onscroll = () => {
-    
-    if (window.scrollY > 500) {
-        refs.scrollBtn.classList.remove('is-hidden');
-    }
-    else if (window.scrollY < 500) {
-        refs.scrollBtn.classList.add('is-hidden');
-    }
-}
+  if (window.scrollY > 500) {
+    refs.scrollBtn.classList.remove('is-hidden');
+  } else if (window.scrollY < 500) {
+    refs.scrollBtn.classList.add('is-hidden');
+  }
+};
 
 refs.scrollBtn.onclick = () => {
-    window.scrollTo(0, 0);
-}
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
