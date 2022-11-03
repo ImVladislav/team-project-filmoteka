a5957af0718406ea137465c9746c6007c625464d
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-11-03 15:54:42 +0200

    pre finish

--- src/js/utilitiesJS/markupCast.js
+++ src/js/utilitiesJS/markupCast.js
@@ -2,37 +2,36 @@ import * as basicLightbox from 'basiclightbox';
 
 import { serverApi } from './serverApi';
 import { spinnerPlay, spinnerStop } from '../spinner';
+import image from '../../images/poster-modal-plug-desktop.jpg';
 
 export const markupCast = async id => {
-  //   spinnerPlay();
+  spinnerPlay();
   const data = await serverApi.getCasts(id);
 
-  //   spinnerStop();
-  let casts = [];
+  const casts = data.slice(0, 15);
 
-  if (data.length > 10) {
-    casts = data.slice(0, 10);
-  }
-
-  cast = casts.map(
-    item =>
-      `<li class="cast__item">
-    <img src="${serverApi.baseSrc}${item.profile_path}" class="cast__poster"/>
-    <div class="cast__wrapper">
-    <p class="cast__name">Name: <span>${item.name}</span></p>
-    <p class="cast__popularity">Popularity: <span>${Number.parseInt(
-      item.popularity
-    )}</span></p>
-    <p class="cast__character">Cast: <span>${item.character}</span></p>
-    </div>
-    </li>`
-  );
+  const castMarkup = casts.map(item => {
+    const poster = item.profile_path
+      ? `${serverApi.baseSrc}${item.profile_path}`
+      : image;
+    return `<li class="cast__item">
+        <img src="${poster}" class="cast__poster"/>
+        <div class="cast__wrapper">
+        <p class="cast__name">Name: <span>${item.name}</span></p>
+        <p class="cast__popularity">Popularity: <span>${Number.parseInt(
+          item.popularity
+        )}</span></p>
+        <p class="cast__character">Cast: <span>${item.character}</span></p>
+        </div>
+        </li>`;
+  });
 
   const instance = basicLightbox.create(`
 <div class="cast__container">
-<ul class="cast__list">${cast.join('')}</ul>
+<ul class="cast__list">${castMarkup.join('')}</ul>
 </div>
 `);
 
   instance.show();
+  spinnerStop();
 };
