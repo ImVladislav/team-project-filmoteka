0c7e61438857b0926f91d14a900f8237116d04bc
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2022-11-04 13:51:49 +0200

    some validations fix

--- src/js/onLibraryClick.js
+++ src/js/onLibraryClick.js
@@ -9,6 +9,14 @@ onMyLibraryClick();
 function onMyLibraryClick() {
   serverApi.setlang(JSON.parse(localStorage.getItem('language')));
 
+  if (!localStorage.getItem('watch')) {
+    localStorage.setItem('watch', JSON.stringify([]));
+  }
+
+  if (!localStorage.getItem('queue')) {
+    localStorage.setItem('queue', JSON.stringify([]));
+  }
+
   try {
     const watch = JSON.parse(localStorage.getItem('watch'));
     const queue = JSON.parse(localStorage.getItem('queue'));
